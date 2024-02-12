70eb0481177ce23af5465fcb9ba39e128147cf98
prabhat1510 <prabhatc@gmail.com>
2024-02-12 14:29:46 +0530

    Restful Api example

--- bookapp/src/main/java/com/blueyonder/bookapp/service/UsersServiceImpl.java
+++ bookapp/src/main/java/com/blueyonder/bookapp/service/UsersServiceImpl.java
@@ -1,9 +1,13 @@
 package com.blueyonder.bookapp.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.blueyonder.bookapp.entities.Role;
 import com.blueyonder.bookapp.entities.Users;
+import com.blueyonder.bookapp.exceptions.RoleNotFoundException;
 import com.blueyonder.bookapp.exceptions.UserNameNotFoundException;
 import com.blueyonder.bookapp.exceptions.UserNotFoundException;
 import com.blueyonder.bookapp.repositories.UsersRepository;
@@ -15,19 +19,22 @@ public class UsersServiceImpl implements UsersService {
 	private UsersRepository usersRepo;
 	@Override
 	public Users addUsers(Users users) {
-		// TODO Auto-generated method stub
-		return null;
+		return usersRepo.save(users);
 	}
 
 	@Override
 	public Users getUsersById(Integer userId) throws UserNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Users> users = usersRepo.findById(userId);
+		if (users.isPresent()) {
+			return users.get();
+		} else {
+			throw new UserNotFoundException();
+		}
 	}
 
 	@Override
 	public Users getUsersByName(String userName) throws UserNameNotFoundException {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
