24a3bf8b17a4c2da4ef79e53e1755dd004e68d7e
prabhat1510 <prabhatc@gmail.com>
2024-02-20 11:54:31 +0530

    Spring Security Example

--- springsecurityexample/src/main/java/com/blueyonder/springsecurityexample/configuration/SecurityConfiguration.java
+++ springsecurityexample/src/main/java/com/blueyonder/springsecurityexample/configuration/SecurityConfiguration.java
@@ -1,10 +1,41 @@
 package com.blueyonder.springsecurityexample.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		
+		http.authorizeHttpRequests((authorize) -> authorize
+						.anyRequest().authenticated())
+			.httpBasic(withDefaults())
+			.formLogin(withDefaults());
 
+		return http.build();
+	}
+
+
+	@SuppressWarnings("deprecation")
+	@Bean
+	public InMemoryUserDetailsManager userDetailsService() {
+		UserDetails user = User.withDefaultPasswordEncoder()
+				.username("user")
+				.password("password")
+				.roles("USER")
+				.build();
+		return new InMemoryUserDetailsManager(user);
+	}
 }
